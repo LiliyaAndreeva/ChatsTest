f90483355a3f3c87cd83dd5a3abe838c12c50f00
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2024-02-16 16:51:30 +0500

    Initial Commit

---
+++ ChatsTest/ChatsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsTestApp.swift
+//  ChatsTest
+//
+//  Created by Лилия Андреева on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
